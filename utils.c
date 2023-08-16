3f5a939d39d60dcbeb5add144e38d4fec5cfacf4
robalb <11277482+robalb@users.noreply.github.com>
2023-08-17 01:15:17 +0200

    added tests framework

--- utils.c
+++ utils.c
@@ -29,3 +29,40 @@ void printState(uint8_t state[]){
 void generatePool(struct state *pool){
 
 }
+
+
+/*
+* test suite globals
+*/
+char failed_assert_descr[MAX_FAIL][MAX_TEST_NAME];
+int test_count = 0;
+int test_fail = 0;
+
+void assert_true(char *name, bool test){
+  if(!test){
+    //save the test name if there is enough space
+    if(test_fail < MAX_FAIL){
+      strncpy(failed_assert_descr[test_fail], name, MAX_TEST_NAME-1);
+    }
+    test_fail++;
+  }
+  test_count++;
+}
+void assert_false(char *name, bool test){
+  assert_true(name, !test);
+}
+
+void print_test_results(){
+  printf("==== FAILING =====\n");
+  for(int i=0; i< MAX_FAIL; i++){
+    if(strlen(failed_assert_descr[i]) == 0)
+      break;
+    printf("%s... FAILED\n", failed_assert_descr[i]);
+  }
+  if(test_fail > MAX_FAIL){
+    printf("%d more FAILED (not enough space to store their name)\n", test_fail - MAX_FAIL);
+  }
+  printf("==================\n");
+  printf("%d/%d passed\n", test_count-test_fail, test_count);
+}
+
--- utils.h
+++ utils.h
@@ -52,4 +52,13 @@ void printState(uint8_t state[]);
 void generatePool(struct state *pool);
 
 
+/*
+ * Homemade test framework
+ */
+#define MAX_FAIL 100
+#define MAX_TEST_NAME 30
+void assert_true(char *name, bool test);
+void assert_false(char *name, bool test);
+void print_test_results();
+
 #endif
